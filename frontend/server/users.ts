780269d1e247ef0854ddd0dba46fff992d19fbdb
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-05 14:01:01 +0100

    The env variable bug is fixed

--- frontend/server/users.ts
+++ frontend/server/users.ts
@@ -1,7 +1,7 @@
 "use server";
 
 export const get = async () => {
-    const data = await fetch(`${process.env.APP_URL/users}`);
+    const data = await fetch(`${process.env.APP_URL}/users`);
     const json = await data.json();
     return json.data();
 };
