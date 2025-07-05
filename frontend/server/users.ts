33bbf52358641a902789294c8a721c297fb079c8
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-05 13:59:50 +0100

    The fetch data is finally returned

--- frontend/server/users.ts
+++ frontend/server/users.ts
@@ -3,5 +3,5 @@
 export const get = async () => {
     const data = await fetch(`${process.env.APP_URL/users}`);
     const json = await data.json();
-    console.log(json);
+    return json.data();
 };
