3fb78992a0bed80b424f46be21fba1d432eecd52
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-05 14:01:58 +0100

    The user interface is created for the typescript type validation

--- frontend/server/users.ts
+++ frontend/server/users.ts
@@ -1,5 +1,10 @@
 "use server";
 
+interface User {
+    email: string;
+    id: string;
+}
+
 export const get = async () => {
     const data = await fetch(`${process.env.APP_URL}/users`);
     const json = await data.json();
